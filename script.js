3139f09c3e6cd94bea939cb6ba100c24ef38fa81
Nitkhar <43068349+Nitkhar@users.noreply.github.com>
2021-01-07 20:43:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function YOZH() {
+    let yozh_img = document.getElementById('YOZH_img');
+    yozh_img.src = "./images/yozh.jpg";
+}
+
+function YOZH_PRO() {
+    let yozh_img = document.getElementById('YOZH_img');
+    yozh_img.src = "./images/yozhPro.jpg";
+}
+
+function YOZH_PRO_MAX() {
+    let yozh_img = document.getElementById('YOZH_img');
+    yozh_img.src = "./images/yozhProMax.jpg";
+}
