ee76151672ad4d4afb6407ab749a0473c05261c8
Sinichenko Konstantin <k.sinichenko@yandex.by>
2021-01-08 23:57:57 +0300

    Add about page

--- js/script.js
+++ js/script.js
@@ -30,3 +30,43 @@ function YOZH_PRO_MAX() {
     none_another_yozh_text.style.display = "none";
     yozh_img.src = "./images/yozhProMax.jpg";
 }
+
+function close_onclick(id) {
+  document.getElementById(id).style.display = "none"
+}
+
+var list = ["images/main.jpg", "images/yozh.jpg","images/yozhPro.jpg","images/yozhProMax.jpg","images/yozh_0.jpg","images/yozh_0.jpg"]
+var to = -1
+
+function show_galery(index) {
+  document.getElementById('galery').style.display = "block"
+  to = index
+  var obj = document.getElementById("galery_img")
+  obj.src = list[to]
+}
+
+function right_arrow() {
+  var obj = document.getElementById("galery_img")
+  if (to < list.length - 1) {
+    to++
+  } else {
+    to = 0
+  }
+  obj.src = list[to]
+}
+
+function left_arrow() {
+  var obj = document.getElementById("galery_img");
+  if (to > 0) {
+    to--
+  } else {
+    to = list.length - 1
+  }
+  obj.src = list[to]
+}
+
+window.onclick = function(event) {
+  let galery = document.getElementById(`galery`)
+  if(event.target == galery)
+    galery.style.display = "none"
+}
