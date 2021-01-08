c966c1e1e41b2a3f009247f0c9e3356c966b5d1e
Nitkhar <43068349+Nitkhar@users.noreply.github.com>
2021-01-08 05:49:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 function YOZH() {
     let yozh_img = document.getElementById('YOZH_img');
+    let yozh_text = document.getElementById('text_YOZH');
+    let none_yozh_text = document.getElementById('text_YOZH_PRO');
+    let none_another_yozh_text = document.getElementById('text_YOZH_PRO_MAX');
+    yozh_text.style.display = "block";
+    none_yozh_text.style.display = "none";
+    none_another_yozh_text.style.display = "none";
     yozh_img.src = "./images/yozh.jpg";
 }
 
 function YOZH_PRO() {
     let yozh_img = document.getElementById('YOZH_img');
+    let yozh_text = document.getElementById('text_YOZH_PRO');
+    let none_yozh_text = document.getElementById('text_YOZH');
+    let none_another_yozh_text = document.getElementById('text_YOZH_PRO_MAX');
+    yozh_text.style.display = "block";
+    none_yozh_text.style.display = "none";
+    none_another_yozh_text.style.display = "none";
     yozh_img.src = "./images/yozhPro.jpg";
 }
 
 function YOZH_PRO_MAX() {
     let yozh_img = document.getElementById('YOZH_img');
+    let yozh_text = document.getElementById('text_YOZH_PRO_MAX');
+    let none_yozh_text = document.getElementById('text_YOZH_PRO');
+    let none_another_yozh_text = document.getElementById('text_YOZH');
+    yozh_text.style.display = "block";
+    none_yozh_text.style.display = "none";
+    none_another_yozh_text.style.display = "none";
     yozh_img.src = "./images/yozhProMax.jpg";
 }
