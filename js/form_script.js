8f372397e0d4cd45a93ae3b331393b546d437d81
Sinichenko Konstantin <k.sinichenko@yandex.by>
2021-01-10 18:33:50 +0300

    Add map

--- js/form_script.js
+++ js/form_script.js
@@ -1,27 +1,26 @@
-const makeLog = (Name, descr) => (
+const makeLog = (name, descr) => (
     new Map([
-        ['Name', Name],
+        ['name', name],
         ['descr', descr],
     ])
 );
 
 function send() {
-var Name = document.getElementById('Name').value;
-var tel = document.getElementById('tel').value;
-var email = document.getElementById('email').value;
-var hedg = document.getElementById('hedg').value;
-var color = document.getElementById('color').value;
-var address = document.getElementById('address').value;
-var dop_info = document.getElementById('dop_info').value;
-var descr = tel + "\n" + email + "\n" + hedg + "\n" + color + "\n" + address + "\n" + dop_info
-console.log(makeLog(Name, descr));
+  var name = document.getElementById('name').value;
+  var tel = document.getElementById('tel').value;
+  var email = document.getElementById('email').value;
+  var hedg = document.getElementById('hedg').value;
+  var color = document.getElementById('color').value;
+  var address = document.getElementById('address').value;
+  var info = document.getElementById('info').value;
+  var purchase = new Map([['name', name], ['tel', tel], ['tel', tel], ['email', email], ['hedg', hedg], ['color', color], ['address', address], ['info', info]])
+  console.log(purchase);
 
-
-document.getElementById('Name').value = '';
-document.getElementById('tel').value = '';
-document.getElementById('email').value = '';
-document.getElementById('hedg').value = "YOZH";
-document.getElementById('color').value ="Nature";
-document.getElementById('address').value ='';
-document.getElementById('dop_info').value ='';
+  document.getElementById('name').value = '';
+  document.getElementById('tel').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('hedg').value = "YOZH";
+  document.getElementById('color').value ="Nature";
+  document.getElementById('address').value ='';
+  document.getElementById('info').value ='';
 }
